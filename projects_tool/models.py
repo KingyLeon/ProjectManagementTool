e51e8dae07314a0e107836a5bf105671595a4b24
KingyLeon <kingyleon2004@gmail.com>
2024-10-15 13:07:33 +0100

    Model adjustments to fit criteria more appropriately

--- projects_tool/models.py
+++ projects_tool/models.py
@@ -1,6 +1,8 @@
 from django.db import models
-from django.core.validators import MinValueValidator, MaxValueValidator, ValidationError, RegexValidator
-
+from django.template.defaultfilters import slugify
+from django.core.validators import MaxValueValidator, RegexValidator
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 # Validators
 def divisible_by_five(value):
@@ -11,16 +13,14 @@ def divisible_by_five(value):
 
 
 class Project(models.Model):
-    title = models.CharField(blank=False, unique=True, max_length=64)
-    description = models.CharField(max_length=256, blank=True)
-    imageField = models.ImageField(upload_to="media/", blank=True)
-    slugField = models.SlugField(null=False, blank=True)
+    title = models.CharField(unique=True, max_length=64)
+    description = models.TextField(max_length=256, blank=True, null=True)
+    imageField = models.ImageField(upload_to="media", blank=True, null=True)
+    slugField = models.SlugField(null=False, unique=True)
 
     def save(self, *args, **kwargs):
-        if not self.slugField:
-            self.slugField = self.title.lower().replace(' ', '-')
-        super(Project, self).save(*args, **kwargs)
-
+        self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
 
 class Board(models.Model):
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
@@ -32,12 +32,11 @@ class Board(models.Model):
 
 class List(models.Model):
     board = models.ForeignKey(Board, on_delete=models.CASCADE, null=False)
-    title = models.CharField(blank=False, null=False, max_length=64)
-    position = models.IntegerField(validators=[MinValueValidator(0)])
+    title = models.CharField(max_length=64)
+    position = models.PositiveIntegerField()  
 
     class Meta:
         unique_together = ("board", "title")
-
 
 class Label(models.Model):
     title = models.CharField(blank=False, max_length=32)
@@ -48,21 +47,24 @@ class Label(models.Model):
     class Meta:
         unique_together = ("title", "project")
 
-
 class Task(models.Model):
-    class Priorities(models.TextChoices):
-        HIGH = 'HIGH', 'High'
-        MEDIUM = 'MEDIUM', 'Medium'
-        LOW = 'LOW', 'Low'
+    class Priority(models.TextChoices):
+        HIGH = 'H', _('High')
+        MEDIUM = 'M', _('Medium')
+        LOW = 'L', _('Low')
 
-    task_no = models.IntegerField(primary_key=True)
+    def validate_story_points(value):
+        if value % 5 != 0 or value > 100:
+            raise ValidationError(
+                _('%(value)s is not a valid story point value'),
+                params={'value': value})
+
+    task_no = models.AutoField(primary_key=True)
+
     list = models.ForeignKey(List, on_delete=models.CASCADE)
-    title = models.CharField(blank=False, max_length=64)
-    description = models.CharField(max_length=512, blank=True)
-
-    priority = models.CharField(
-        choices=Priorities.choices, default=Priorities.MEDIUM, max_length=6)
-
-    story_points = models.IntegerField(
-        validators=[MinValueValidator(0), MaxValueValidator(100), divisible_by_five])
-    labels = models.ManyToManyField(Label)
+    title = models.CharField(max_length=64)
+    description = models.TextField(max_length=512, blank=True, null=True)
+    priority = models.CharField(max_length=1, choices=Priority)
+    story_points = models.PositiveIntegerField(
+        validators=[validate_story_points])
+    labels = models.ManyToManyField('Label')
